b61cafeeecb14aca605dc1edcca8c5b386e99dad
Mohamed Mostafa <mohaamed.mostafa0@gmail.com>
2026-09-13 17:50:32 +0300

    Create student profile

---
+++ main.py
@@ -0,0 +1,8 @@
+name = "Mohamed" 
+major = "BIS" 
+university = "Assiut University" 
+print("Student Profile") 
+print("----------------") 
+print("Name:", name) 
+print("Major:", major) 
+print("University:", university)
